4e586c6e6befdf68aa314db1c4d5952375c8fd53
Louison GILLET <louison.gillet@etu.umontpellier.fr>
2024-10-12 20:42:06 +0200

    Création Rmd et multiples ajouts

--- R/script.R
+++ R/script.R
@@ -1,43 +1,61 @@
 # En tête ---- 
 
-#Desc : Prépa TP1 ADM
+#Desc : DM1 ADM
 #Date: 02/10/2024
-#Auteur : Louison GILLET
+#Auteur : EL MAZZOUJI Wahel & GILLET Louison 
 
-# Partie 1 : dataframe ---- 
+# Dataframe ---- 
 
-Datagenus <- read.csv("~/Louison/M1/ADM/DM1/data/Datagenus.csv", sep=";")
+Datagenus <- read.csv("data/Datagenus.csv", sep=";")
+#str(Datagenus)
+data <- Datagenus[1:1000,] # On ne prend pas la ligne 1001 
 
-# On enlève la ligne 1001 
-data <- Datagenus[1:1000,]
-str(data)
-summary(data)
-head(data)
+# Partie 1 ---- 
 
-##### Question 1 #### 
+### 1.0 Sélection des colonnes des espèces
 
-# Sélectionner les colonnes des espèces
 especes <- paste0("gen", 1:27)
-#especes
 
-# Calculer la densité de peuplement pour chaque espèce
-densite_especes <- data[especes] / data$surface
-head(densite_especes)
+#### 1.1 Calcul de la densité de peuplement pour chaque espèce (gen1 à gen27) ####
 
-# Moyenne des densités par parcelle 
-densite_especes$densite_moyenne <- rowMeans(densite_especes)
-head(densite_especes)
+densite_peuplement <- as.matrix(data[especes] / data$surface) # Conversion en matrice 
 
-# Centrer les densités 
-densite_c <- densite_especes - densite_especes$densite_moyenne
-head(densite_c)
-rowMeans(densite_c)
+#### 1.2 Centrage et réduction avec des opérations matricielles ####
 
-# Réduire les densités 
-densite_cr <- densite_c / sd
+### Calcul des moyennes pour chaque espèce (colonne)
 
-# Calculer l'inertie totale du nuage de points
-inertie_totale <- sum(densite_cr^2) 
-inertie_totale  # Doit être proche de 27
+moyennes_especes <- (colMeans(densite_peuplement))
+
+### Calcul des écarts-types pour chaque espèce (colonne) 
+n <- nrow(densite_peuplement)
+p <- ncol(densite_peuplement)
+mat_moyenne <- matrix(moyennes_especes, nrow = n, ncol = p, byrow = TRUE)
+#remplit chaque ligne avec la densité de la colonne 
+
+sd_especes <- sqrt(colSums((densite_peuplement - mat_moyenne)^2) / (n - 1)) #racine de la variance sans-biais 
+
+### Centrage et réduction : on soustrait les moyennes et on divise par l'écart-type
+
+mat_sd <- matrix(sd_especes, nrow = n, ncol = p, byrow = TRUE)
+#idem avec l'écart-type 
+
+densite_centree_reduite <- (densite_peuplement - mat_moyenne) / mat_sd 
+
+#### 1.3 Barycentre et inertie #### 
+
+### 1.3.1 Barycentre à l'origine (moyennes des colonnes proches de 0)
+#summary(densite_centree_reduite)
+moyennes_apres_centrage <- colMeans(densite_centree_reduite)
+test_barycentre_a_l_origine <- all(abs(moyennes_apres_centrage) < 1e-10)
+test_barycentre_a_l_origine
+
+### 1.3.2 Inertie totale (variances des colonnes proches de 1)
+# Variance par colonne 
+variances_apres_centrage <- colSums(densite_centree_reduite^2) / (n-1)
+#variances_apres_centrage
+
+# Inertie totale = somme des variances
+inertie_totale <- sum(variances_apres_centrage)
+#inertie_totale 
 
 
